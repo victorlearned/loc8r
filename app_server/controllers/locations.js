90ab745e0a3238f628b8726c597bddc03da63e2c
Victor Learned <victorlearned@gmail.com>
2017-01-19 17:02:44 -0800

    updating rest of the views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -31,10 +31,59 @@ module.exports.homelist = function(req, res) {
 
 /* Get 'Location info' page */
 module.exports.locationInfo = function(req, res) {
-  res.render('location-info', { title: 'Location info' });
+  res.render('location-info', {
+    title: 'Ballard Coffee Works',
+    pageHeader: {
+      title: 'Ballard Coffee Works'
+    },
+    sidebar: {
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get work done.',
+      callToAction: 'If you\'ve been and you like it - or you don\' - please leave a review to help others.'
+    },
+    location: {
+      name: 'Ballard Coffee Works',
+      address: '2060 Market Street, Seattle, WA 98107',
+      rating: 4,
+      facilities: ['Quiet', 'Beer', 'Wifi'],
+      coords: {
+        lat: 51.455041,
+        lng: -0.9690884
+      },
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        close: false
+      }, {
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false
+      }, {
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Simon Holmes',
+        rating: 5,
+        timestamp: '16 July 2013',
+        reviewText: 'What a great place. I can\'t say enough good things about it.'
+      }, {
+        author: 'Victor Learned',
+        rating: 3,
+        timestamp: '19 January 2017',
+        reviewText: 'Was okay. Wifi isn\'t the best'
+      }]
+    }
+  });
 };
 
 /* Get 'Add review' page */
 module.exports.addReview = function(req, res) {
-  res.render('location-review-form', { title: 'Add review' });
+  res.render('location-review-form', {
+    title: 'Review Ballard Coffee Works on Loc8',
+    pageHeader: {
+      title: 'Review Ballard Coffee Works'
+    }
+  });
 };
